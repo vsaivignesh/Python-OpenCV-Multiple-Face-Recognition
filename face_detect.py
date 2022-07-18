3fa6404c47e0aaefe09a7c187b7e0392f311715f
vsaivignesh <49581269+vsaivignesh@users.noreply.github.com>
2022-07-18 12:31:11 +0530

    update
    python 3 and colab compat

--- face_detect.py
+++ face_detect.py
@@ -1,11 +1,13 @@
 import cv2
 import sys
+from google.colab.patches import cv2_imshow
+from PIL import Image
 
 def face_detect(imagePath):
 	# HaarCascade file, to detect the face.
-	faceCascade = cv2.CascadeClassifier("opencv-files/haarcascade_frontalface_alt.xml")
+	faceCascade = cv2.CascadeClassifier("/content/Python-OpenCV-Multiple-Face-Recognition/opencv-files/haarcascade_frontalface_alt.xml")
 	image = cv2.imread(imagePath)
-	#Convert image to grayscale
+  #Convert image to grayscale
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	grays = []
 	faces = faceCascade.detectMultiScale(
@@ -20,13 +22,13 @@ def face_detect(imagePath):
 		cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 	
 	# Show Detected faces.
-	cv2.imshow("Faces found", image)
+	cv2.imwrite('/content/Python-OpenCV-Multiple-Face-Recognition/result.jpg', image)
 	cv2.waitKey(1)
 	
 	# Append the detected faces into grays list.
 	for i in range(0, len(faces)):
 		(x, y, w, h) = faces[i]
 		grays.append(gray[y:y+w, x:x+h])
-	print "------------------------------------------------------------"
-	print "Detecting Face -\-"
+	print("------------------------------------------------------------")
+	print("Detecting Face -\-")
 	return grays, faces, len(faces)
